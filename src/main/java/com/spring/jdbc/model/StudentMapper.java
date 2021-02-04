9e632d2ac85bf5f7f9a1aec32dacd2b240aa97d0
Talyson Moreira <talyson.penha@gmail.com>
2021-02-04 19:02:23 -0300

    StudentMapper implemented

---
+++ src/main/java/com/spring/jdbc/model/StudentMapper.java
@@ -0,0 +1,20 @@
+package com.spring.jdbc.model;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+public class StudentMapper implements RowMapper<Student> {
+
+	@Override
+	public Student mapRow(ResultSet resultSet, int i) throws SQLException {
+		Student student = new Student();
+		student.setId(resultSet.getInt("id"));
+		student.setName(resultSet.getString("name"));
+		student.setAge(resultSet.getInt("age"));
+		student.setWeight(resultSet.getInt("weight"));
+		
+		return student;
+	}
+}
---
+++ src/main/java/com/spring/rest/jdbc/dao/StudentDAO.java
@@ -0,0 +1,5 @@
+package com.spring.rest.jdbc.dao;
+
+public class StudentDAO {
+
+}
