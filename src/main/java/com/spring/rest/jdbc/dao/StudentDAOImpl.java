57a367a60195854f393deb7203eb750f8b97206a
Talyson Moreira <talyson.penha@gmail.com>
2021-02-04 20:20:06 -0300

    API ready

--- src/main/java/com/spring/rest/jdbc/dao/StudentDAOImpl.java
+++ src/main/java/com/spring/rest/jdbc/dao/StudentDAOImpl.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Component;
 
 import com.spring.jdbc.model.Student;
